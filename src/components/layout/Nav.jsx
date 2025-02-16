a4c91a3804573dcf0b6a2120f95046b8d94f9632
Suzowy <suzowy@gmail.com>
2025-02-16 23:29:17 +0100

    header

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,37 +1,18 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 
 const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <header>
-      {/* Checkbox oculto para controlar el menú */}
-      <input 
-        type="checkbox" 
-        id="checkbox" 
-        checked={isOpen} 
-        onChange={() => setIsOpen(!isOpen)} 
-      />
-
-      {/* Botón de menú hamburguesa */}
-      <label htmlFor="checkbox" className="menu-toggle">
-        <div className={`bars ${isOpen ? "open-bar1" : ""}`} id="bar1"></div>
-        <div className={`bars ${isOpen ? "open-bar2" : ""}`} id="bar2"></div>
-        <div className={`bars ${isOpen ? "open-bar3" : ""}`} id="bar3"></div>
-      </label>
-
-      {/* Menú de navegación */}
-      <nav className={isOpen ? "open" : ""}>
-        <ul>
-          <li><Link to="/inicio" onClick={() => setIsOpen(false)}>Inicio</Link></li>
-          <li><Link to="/articulos" onClick={() => setIsOpen(false)}>Artículos</Link></li>
-          <li><Link to="/crear" onClick={() => setIsOpen(false)}>Crear Artículos</Link></li>
-          <li><Link to="/about" onClick={() => setIsOpen(false)}>Quiénes Somos</Link></li>
-        </ul>
+    return (
+      <nav className="nav">
+      <ul>
+          <li><Link to="/inicio">inicio</Link></li>
+          <li> <Link to="/articulos">articulos</Link></li>
+          <li><Link to="/crear">Crear articulos</Link></li>
+          <li><Link to="/about">Quienes somos </Link></li>
+      </ul> 
       </nav>
-    </header>
+ 
   );
 };
 

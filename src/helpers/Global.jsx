116f8c27d0959f3aefff6f382c5ab8df087ede0a
Suzowy <suzowy@gmail.com>
2025-02-17 18:48:56 +0100

    global

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,12 +1,6 @@
 
 const Global = {
-    url: "http://localhost:3900/api/"
-
-
-  //   url:'https://backend-proyectos-eq91.onrender.com/api'
-
-  //  url:'mongodb+srv://admin:admin@proyecto.ii5axio.mongodb.net/'
-
+    url: "https://back-blog-six.vercel.app/api/"
   };
 
   export default Global;
